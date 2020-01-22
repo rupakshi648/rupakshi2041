82520810614d4d9d6ec5c4d01cd05192253cdcae
rupakshi648 <59842424+rupakshi648@users.noreply.github.com>
2020-01-22 14:47:08 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,101 @@
+//  console.log("hello world");
+// console.log(bob);
+// const arr=["o" ,"o" ,"o"];
+// let myfriend ="bob"
+// const obj={
+// name:[""],
+// age: 27
+// gender:
+// class:["sodvy","tech1101"]
+// }
+
+
+
+//  function relationship(bf,gf)
+//  {
+//  return bf + "is dating" + gf;
+//  } 
+//  console.log(relationship("mandeep","ashley"));
+
+ 
+// function add0ne(num)
+
+
+
+
+// const jannet = function()
+//   const name = "jon";
+//   const age
+//   jannet()
+//   console.log(addmultiples(8));
+//   return 5 * num;
+
+
+
+
+// // function hello world():
+// // return "hello world!";
+
+
+// // function colour(color)
+// // return colour;
+// // console.log(iscolour("blue"))
+// //   let mybrother =
+// function multiply(num1,num2)
+// {
+// const total num1*num2;
+// }
+// console.log(total);
+// multiply(3,4);
+
+// const multi = (param1,param2) =>{
+//     returnparam1*param2
+//     };
+//     console.log(multi(4,5))
+
+//     const multi2 = function(param1,param2)  {
+//         return param1*param2;
+
+
+
+
+
+//         function words(w1,w2){
+//         return w1 + " " + w2;
+//         }
+//         console.log(words("hello","world"));
+
+        const sisters = function(param1,param2,param3){
+            return param1 + "and"+ param2  + "are sisters" + param3
+            }
+            console.log(sisters("jenny","liza","?"));
+
+                
+
+
+             function harman(a1,a2)
+             {
+
+              return a1*a2;
+
+             }
+                 console.log(harman(4,5));   
+/////////////////////////////////////////////////////
+
+            const  mandeep = function(b1, b2 , b3 )
+            {
+                return b1*b2+b3;
+            }
+            console.log(mandeep(2,3,4));    
+
+            // string 
+               const bowvalley = function(c1,c2,c3)
+               {
+                   return c1 +"     is dating "+ c2 +"  "+ c3 ;
+
+
+               }
+
+                 console.log(bowvalley("mandeep" , "preeti" , "@"));
+
+
